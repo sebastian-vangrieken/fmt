1229fa191a973e1cc72246ca45f9649cc5465dc1
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-01-27 16:06:10 +0100

    perfect square with utf-8 characters xD

--- main.c
+++ main.c
@@ -10,28 +10,28 @@ int main(int argc, char** argv) {
 
     for (int i = 1; i < ws.ws_row; i++) {
         if (i == 1) {
-            printf("\u2502");
+            printf("\u250c");
             for (int j = 1; j < ws.ws_col - 1; j++) {
                 if (j == middle) {
-                    printf("\u2502");
+                    printf("\u252c");
                 }
                 else {
                     printf("\u2500");
                 }
             }
-            printf("\u2502\n");
+            printf("\u2510\n");
         }
         else if (i == ws.ws_row - 1) {
-            printf("\u2502");
+            printf("\u2514");
             for (int j = 1; j < ws.ws_col - 1; j++) {
                 if (j == middle) {
-                    printf("\u2502");
+                    printf("\u2534");
                 }
                 else {
                     printf("\u2500");
                 }
             }
-            printf("\u2502\n");
+            printf("\u2518\n");
         }
         else {
             printf("\u2502");
