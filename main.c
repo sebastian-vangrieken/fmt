18c85131fbba94d0f522cc5cfa7969ccf392a6f3
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-01-29 12:35:52 +0100

    testing io

--- main.c
+++ main.c
@@ -3,19 +3,38 @@
 #include "getcwd.h"
 
 int main(int argc, char** argv) {
-    // printf("%s\n", get_current_working_dir());
-    if (argc == 1) {
-        printf("no options given\n");
+    char input[20];
 
-        return 0;
-    }
+    int i = 0;
+    while (1) {
+        fgets(input, sizeof(input), stdin);
 
-    if (strcmp(argv[1], "-e") == 0) {
-        printf("option %s given\n", argv[1]);
-    }
-    else {
-        printf("Unknown options: %s\n", argv[1]);
+        if (strcmp(input, ":q\n") == 0) {
+            break;
+        }
+
+        printf("%d\n", i);
+        i++;
     }
 
     return 0;
 }
+
+// if (argc == 1) {
+//     printf("no options given\n");
+
+//     return 0;
+// }
+
+// if (strcmp(argv[1], "-e") == 0) {
+//     printf("option %s given\n", argv[1]);
+// }
+// else {
+//     printf("Unknown options: %s\n", argv[1]);
+// }
+
+// printf("argc: %d\n", argc);
+// for (int i = 0; i < argc; i++) {
+//     printf("argv[%d]: %s\n", argc, argv[i]);
+// }
+// printf("%s\n", get_current_working_dir());
